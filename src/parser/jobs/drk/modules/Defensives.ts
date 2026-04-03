f84e76beba2dd4d1a66a7536020be6f9d82005df
ddh404 <197553143+ddh404@users.noreply.github.com>
2026-04-03 18:09:16 +0800

    update DRK/GNB defensives

--- src/parser/jobs/drk/modules/Defensives.ts
+++ src/parser/jobs/drk/modules/Defensives.ts
@@ -2,10 +2,22 @@ import {Defensives as CoreDefensives} from 'parser/core/modules/Defensives'
 
 export class Defensives extends CoreDefensives {
 	protected override trackedActions = [
+		this.data.actions.THE_BLACKEST_NIGHT,
 		this.data.actions.DARK_MIND,
 		this.data.actions.LIVING_DEAD,
 		this.data.actions.DARK_MISSIONARY,
 		this.data.actions.OBLATION,
+		this.data.actions.SHADOW_WALL,
 		this.data.actions.SHADOWED_VIGIL,
 	]
+
+	protected override actionLevelGates = {
+		[this.data.actions.THE_BLACKEST_NIGHT.id]: {minLevel: 70},
+		[this.data.actions.DARK_MIND.id]: {minLevel: 45},
+		[this.data.actions.LIVING_DEAD.id]: {minLevel: 50},
+		[this.data.actions.DARK_MISSIONARY.id]: {minLevel: 66},
+		[this.data.actions.OBLATION.id]: {minLevel: 82},
+		[this.data.actions.SHADOW_WALL.id]: {minLevel: 38, maxLevel: 91},
+		[this.data.actions.SHADOWED_VIGIL.id]: {minLevel: 92},
+	}
 }
--- src/parser/jobs/gnb/modules/Defensives.ts
+++ src/parser/jobs/gnb/modules/Defensives.ts
@@ -5,10 +5,23 @@ export class Defensives extends CoreDefensives {
 	static override displayOrder = DISPLAY_ORDER.DEFENSIVES
 	protected override trackedActions = [
 		this.data.actions.SUPERBOLIDE,
+		this.data.actions.NEBULA,
 		this.data.actions.GREAT_NEBULA,
 		this.data.actions.HEART_OF_LIGHT,
+		this.data.actions.HEART_OF_STONE,
 		this.data.actions.HEART_OF_CORUNDUM,
 		this.data.actions.AURORA,
 		this.data.actions.CAMOUFLAGE,
 	]
+
+	protected override actionLevelGates = {
+		[this.data.actions.SUPERBOLIDE.id]: {minLevel: 50},
+		[this.data.actions.NEBULA.id]: {minLevel: 38, maxLevel: 91},
+		[this.data.actions.GREAT_NEBULA.id]: {minLevel: 92},
+		[this.data.actions.HEART_OF_LIGHT.id]: {minLevel: 64},
+		[this.data.actions.HEART_OF_STONE.id]: {minLevel: 68, maxLevel: 81},
+		[this.data.actions.HEART_OF_CORUNDUM.id]: {minLevel: 82},
+		[this.data.actions.AURORA.id]: {minLevel: 45},
+		[this.data.actions.CAMOUFLAGE.id]: {minLevel: 6},
+	}
 }
