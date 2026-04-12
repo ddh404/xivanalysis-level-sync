06f57d0983da919a0546349e9aae7e22e2fb10cf
ddh404 <197553143+ddh404@users.noreply.github.com>
2026-04-12 23:13:50 +0800

    routing: switch to HashRouter for GitHub Pages compatibility

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,14 +2,14 @@ import {App} from 'components/App'
 import {BreadcrumbProvider} from 'components/GlobalSidebar'
 import {I18nLoader} from 'components/I18nLoader'
 import {Provider as TooltipProvider} from 'components/ui/DbLink'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import {StoreProvider} from 'store'
 
 export const Root = () => (
 	<StoreProvider>
 		<I18nLoader>
 			<TooltipProvider>
-				<Router basename={process.env.PUBLIC_URL} future={{
+				<Router future={{
 					v7_relativeSplatPath: true,
 				}}>
 					<BreadcrumbProvider>
