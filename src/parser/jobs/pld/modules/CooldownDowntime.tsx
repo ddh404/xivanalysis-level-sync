e26083856c9f8b76c4b7e2b61d0473cc521bf4db
ddh404 <197553143+ddh404@users.noreply.github.com>
2026-04-14 17:16:09 +0800

    pld: add level gates to cooldown downtime tracking

--- src/parser/jobs/pld/modules/CooldownDowntime.tsx
+++ src/parser/jobs/pld/modules/CooldownDowntime.tsx
@@ -7,26 +7,45 @@ export class CooldownDowntime extends CoreCooldownDowntime {
 			cooldowns: [this.data.actions.FIGHT_OR_FLIGHT],
 			// Standard opener uses after 3rd GCD
 			firstUseOffset: 7500,
+			minLevel: 2,
 		},
 		{
 			cooldowns: [this.data.actions.IMPERATOR],
 			// Standard opener uses after 3rd GCD
 			firstUseOffset: 7500,
+			minLevel: 96,
+		},
+		{
+			cooldowns: [this.data.actions.REQUIESCAT],
+			// Standard opener uses after 3rd GCD
+			firstUseOffset: 7500,
+			minLevel: 68,
+			maxLevel: 95,
 		},
 		{
 			cooldowns: [this.data.actions.EXPIACION],
 			// Standard opener uses after 4th GCD
 			firstUseOffset: 10000,
+			minLevel: 86,
+		},
+		{
+			cooldowns: [this.data.actions.SPIRITS_WITHIN],
+			// Standard opener uses after 4th GCD
+			firstUseOffset: 10000,
+			minLevel: 30,
+			maxLevel: 85,
 		},
 		{
 			cooldowns: [this.data.actions.CIRCLE_OF_SCORN],
 			// Standard opener uses after 4th GCD
 			firstUseOffset: 10000,
+			minLevel: 50,
 		},
 		{
 			cooldowns: [this.data.actions.INTERVENE],
 			// Standard opener uses after 5th GCD
 			firstUseOffset: 12500,
+			minLevel: 66,
 		},
 	]
 }
